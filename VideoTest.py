c79b95ef086a894e7a30fcc32f675dd228c76ae4
ajaichemmanam <ajaichemmanam@gmail.com>
2017-12-11 01:02:18 +0530

    Get RGB and GrayScale frames
    Use Opencv for frame conversion to RGB and Grayscale

--- VideoTest.py
+++ VideoTest.py
@@ -5,6 +5,8 @@ cv2.namedWindow('Emovere')
 video_capture = cv2.VideoCapture(0)
 while(True):
     bgr_image = video_capture.read()[1]
+    gray_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2GRAY)
+    rgb_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2RGB)
     cv2.imshow("Emovere", bgr_image)
     if cv2.waitKey(1) & 0xFF == ord('q'):
             break
