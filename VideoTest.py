ceb762cb3553ab174d3c3b761c850918942b0b6a
ajaichemmanam <ajaichemmanam@gmail.com>
2017-12-11 01:13:17 +0530

    Crop Image to HaarOutput
    Crop Image to Haar Output with some offsets

--- VideoTest.py
+++ VideoTest.py
@@ -7,6 +7,13 @@ def load_haar(path):
 def detect_faces(detection_model, gray_image):
     return detection_model.detectMultiScale(gray_image, 1.3, 5)
 
+def apply_offsets(face_coordinates, offsets):
+    x, y, width, height = face_coordinates
+    x_off, y_off = offsets
+    return (x - x_off, x + width + x_off, y - y_off, y + height + y_off)
+
+face_offsets = (20, 40)
+
 # Path to model
 haar_cascade_path = 'haar/haarcascade_frontalface_default.xml'
 
@@ -21,7 +28,11 @@ while(True):
     gray_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2GRAY)
     rgb_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2RGB)
     faces = detect_faces(face_detection, gray_image)
-    print(faces)   #Print Face-co-ordiates recieved from haar cascade 
+    #print(faces)   #Print Face-co-ordiates recieved from haar cascade
+    for face_coordinates in faces:
+        x1, x2, y1, y2 = apply_offsets(face_coordinates, face_offsets)
+        gray_face = gray_image[y1:y2, x1:x2]
+        cv2.imshow("HaarOutput", gray_face)
     cv2.imshow("Emovere", bgr_image)
     if cv2.waitKey(1) & 0xFF == ord('q'):
             break
